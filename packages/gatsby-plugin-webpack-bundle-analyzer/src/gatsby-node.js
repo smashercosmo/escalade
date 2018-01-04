b3259708c80bf23a57124163d35467ded7b3e3a6
Kennedy Rose <kennedy@kennedyrose.com>
2018-01-04 10:45:41 -0600

    Production option

--- packages/gatsby-plugin-webpack-bundle-analyzer/src/gatsby-node.js
+++ packages/gatsby-plugin-webpack-bundle-analyzer/src/gatsby-node.js
@@ -1,7 +1,11 @@
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 exports.modifyWebpackConfig = ({ config, stage, store }, options) => {
-	if(stage === 'develop'){
+	if(options.disable) return
+	if (
+		stage === 'develop' ||
+		(options.production && stage === 'build-javascript')
+	) {
 		config.plugin('webpack-bundle-analyzer', BundleAnalyzerPlugin, [options])
 	}
 }
