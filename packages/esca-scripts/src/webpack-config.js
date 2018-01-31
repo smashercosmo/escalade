e57e7af5670bb44a11d62a37b3ca31acb7d160af
Kennedy Rose <kennedy@kennedyrose.com>
2018-01-31 08:35:31 -0600

    Config refactor

--- packages/esca-scripts/src/webpack-config.js
+++ packages/esca-scripts/src/webpack-config.js
@@ -10,71 +10,8 @@ export default function(options){
 		plugins: plugins,
 		resolve: {
 			extensions: ['.js', '.jsx', '.json']
-		}
-	}
-
-	const env = options.env || process.env.NODE_ENV
-
-	if (env === 'production') {
-		plugins.push(new webpack.DefinePlugin({
-			'process.env.NODE_ENV': JSON.stringify('production')
-		}))
-		if(options.minify){
-			plugins.push(new UglifyJsPlugin())
-		}
-	}
-	if (env === 'development') {
-		plugins.push(new webpack.HotModuleReplacementPlugin())
-		config.devtool = 'eval'
-	}
-
-	if (options.analyze) {
-		plugins.push(new BundleAnalyzerPlugin())
-	}
-	if (options.cli) {
-		plugins.push(new webpack.BannerPlugin({
-			banner: '#!/usr/bin/env node',
-			raw: true
-		}))
-	}
-
-	if (!options.analyze && !options.browser) {
-		config.target = 'node'
-		config.externals = [nodeExternals()]
-	}
-
-	if (options.browser) {
-		console.log('Building for browser...')
-		plugins.push(
-			new webpack.optimize.ModuleConcatenationPlugin(),
-			new webpack.optimize.OccurrenceOrderPlugin(),
-			new webpack.LoaderOptionsPlugin({
-				minimize: true,
-				debug: false,
-			})
-		)
-		config.module = {
-			rules: [{
-				test: /\.js?$/,
-				use: [{
-					loader: 'babel-loader',
-					options: {
-						presets: [
-							'es2015',
-							'stage-3',
-						],
-						plugins: [
-							'transform-runtime'
-						]
-					},
-				}],
-				include: path.join(process.cwd(), '/'),
-			}]
-		}
-	}
-
-	if(!options.analyze){
-		config.module = {
+		},
+		module: {
 			rules: [{
 				test: /\.js?$/,
 				exclude: /node_modules/,
@@ -90,6 +27,48 @@ export default function(options){
 				include: path.join(process.cwd(), '/'),
 			}]
 		}
+	}
+
+	// Environment specific plugins
+	const env = options.env || process.env.NODE_ENV
+	if (env === 'production') {
+		plugins.push(new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify('production')
+		}))
+		if(options.minify){
+			plugins.push(new UglifyJsPlugin())
+		}
+	}
+	if (env === 'development') {
+		plugins.push(new webpack.HotModuleReplacementPlugin())
+		config.devtool = 'eval'
+	}
+
+	// Option specific plugins
+	if (options.analyze) {
+		plugins.push(new BundleAnalyzerPlugin())
+		delete config.rules
+	}
+	if (options.cli) {
+		plugins.push(new webpack.BannerPlugin({
+			banner: '#!/usr/bin/env node',
+			raw: true
+		}))
+	}
+	if (!options.analyze && !options.browser) {
+		config.target = 'node'
+		config.externals = [nodeExternals()]
+	}
+	if (options.browser) {
+		console.log('Building for browser...')
+		plugins.push(
+			new webpack.optimize.ModuleConcatenationPlugin(),
+			new webpack.optimize.OccurrenceOrderPlugin(),
+			new webpack.LoaderOptionsPlugin({
+				minimize: true,
+				debug: false,
+			})
+		)
 	}
 
 	return config
