3dd31c8f54d25d07fc7893b95f245a1236f03ff5
Kennedy Rose <kennedy@kennedyrose.com>
2018-01-31 09:35:30 -0600

    Create babelrc for tests

--- packages/esca-scripts/src/test.js
+++ packages/esca-scripts/src/test.js
@@ -1,8 +1,17 @@
-import { spawn } from 'child_process'
+import { spawn } from 'child-process-promise'
+import { babel } from '../package.json'
+import { pathExists, outputJson, remove } from 'fs-extra'
 
-export default function(){
-	spawn('mocha --require babel-core/register', [], {
+export default async function(){
+	let rcExists = await pathExists('.babelrc')
+	if(!rcExists){
+		await outputJson('.babelrc', babel)
+	}
+	await spawn('mocha --require babel-core/register', [], {
 		shell: true,
 		stdio: 'inherit',
 	})
+	if(!rcExists){
+		await remove('.babelrc')
+	}
 }
