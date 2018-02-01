a33f3465ad696c118dc338e22d544a8fcbbdc355
Kennedy Rose <kennedy@kennedyrose.com>
2018-02-01 10:14:25 -0600

    Separate help export

--- packages/esca-scripts/README.md
+++ packages/esca-scripts/README.md
@@ -10,34 +10,4 @@ yarn add esca-scripts
 
 ## Usage
 
-```
-Usage
-	$ esca-scripts <command> <options>
-
-Commands
-	$ build     Creates a distribution build
-	$ dev       Opens a development server
-	$ serve     Serves static content
-	$ analyze   Analyze dependency sizes
-	$ test      Runs mocha tests
-
-Options
-	--input    Input path or file
-					Default: ./src/index.js
-	--output   Output path or file
-					Default: [src path]/dist/[src name]
-	--minify   Minifies JavaScript, default: true
-	--env      Change environment variable, default: production
-	--dir      Directory to serve if serving static content
-					Default: ./dist
-	--open     Open development site in browser, default: true
-	--port     Set to specify development site port
-					If not set, the first open port found will be used
-
-Project Options
-	--browser      Set for a browser project
-	--react        Set for a React component
-	--gatsby       Set for a Gatsby project
-	--mobx         Set if including MobX in your React or Gatsby project
-	--cli          Set for a CLI project
-```
+For usage, run `esca-scripts --help` or [view help in source](https://github.com/escaladesports/esca-scripts/blob/master/src/help.js).
--- packages/esca-scripts/src/cli.js
+++ packages/esca-scripts/src/cli.js
@@ -1,39 +1,11 @@
 import meow from 'meow'
+
+import help from './help'
 import webpack from './webpack'
 import serve from './serve'
 import test from './test'
 
-const cli = meow(`
-	Usage
-		$ esca-scripts <command> <options>
-
-	Commands
-		$ build     Creates a distribution build
-		$ dev       Opens a development server
-		$ serve     Serves static content
-		$ analyze   Analyze dependency sizes
-		$ test      Runs mocha tests
-
-	Options
-		--input    Input path or file
-		           Default: ./src/index.js
-		--output   Output path or file
-		           Default: [src path]/dist/[src name]
-		--minify   Minifies JavaScript, default: true
-		--env      Change environment variable, default: production
-		--dir      Directory to serve if serving static content
-		           Default: ./dist
-		--open     Open development site in browser, default: true
-		--port     Set to specify development site port
-		           If not set, the first open port found will be used
-
-	Project Options
-		--browser      Set for a browser project
-		--react        Set for a React component
-		--gatsby       Set for a Gatsby project
-		--mobx         Set if including MobX in your React or Gatsby project
-		--cli          Set for a CLI project
-`, {
+const cli = meow(help, {
 	flags: {
 		banner: {
 			type: 'string'
