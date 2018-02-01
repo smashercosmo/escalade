dc1692a7c74b471db8e4dce6b04e10a82a11e552
Kennedy Rose <kennedy@kennedyrose.com>
2018-02-01 13:11:01 -0600

    Sls logs get first function

--- packages/esca-scripts/src/serverless-logs.js
+++ packages/esca-scripts/src/serverless-logs.js
@@ -1,27 +1,23 @@
 import { readFile } from 'fs-extra'
 import { join } from 'path'
-import yaml from 'yaml'
+import { load } from 'js-yaml'
 
 import spawn from './spawn'
 
 async function getFirstFunction(){
 	let config = await readFile(join(process.cwd(), '/serverless.yml'))
 	config = config.toString()
-	console.log('CONFIG:', config)
-	config = yaml.eval(config)
-	console.log('YAML:', config)
+	config = load(config)
 	return Object.keys(config.functions)[0]
 }
 
 export default async function (options) {
 	let cmd = `serverless logs --tail --stage ${options.stage}`
-
 	let args = []
 	if (args.start){
 		args.push('--startTime', options.start)
 	}
 	args.push('--function', options.function || await getFirstFunction())
-
 	cmd = `${cmd} ${args.join(' ')}`
 	return await spawn(cmd)
 }
