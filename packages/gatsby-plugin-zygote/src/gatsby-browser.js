aaba0f5e9af5278d3973e6e040e93d4994d44d26
Kennedy Rose <kennedy@kennedyrose.com>
2018-03-05 12:39:26 -0600

    Browser and ssr

--- packages/gatsby-plugin-zygote/src/gatsby-browser.js
+++ packages/gatsby-plugin-zygote/src/gatsby-browser.js
@@ -1,8 +1,11 @@
-export function onRouteUpdate({ location }) {
-	if (process.env.NODE_ENV === 'production' && typeof window._hsq === 'object') {
-		// Wait for the title update (see #2478)
-		setTimeout(() => {
-			window._hsq.push(['trackPageView'])
-		}, 0)
+export function onInitialClientRender(a, options) {
+	if('zygote' in window){
+		options = {
+			api: process.env.ZYGOTE_API || process.env.GATSBY_ZYGOTE_API || `https://yh5fc30fhh.execute-api.us-east-1.amazonaws.com/production/handler`,
+			...options
+		}
+		for(let i in options){
+			window.zygote[i] = options[i]
+		}
 	}
 }
--- packages/gatsby-plugin-zygote/src/gatsby-ssr.js
+++ packages/gatsby-plugin-zygote/src/gatsby-ssr.js
@@ -3,7 +3,7 @@ import React from 'react'
 export function onRenderBody({ setPostBodyComponents }, pluginOptions) {
 	if (process.env.NODE_ENV === 'production' && pluginOptions.id) {
 		return setPostBodyComponents([
-			<script key='gatsby-plugin-hubspot' type='text/javascript' id='hs-script-loader' async defer src={`//js.hs-scripts.com/${pluginOptions.id}.js`}></script>
+			<script src='https://deligation--zygote.netlify.com/zygote-v1.js' />
 		])
 	}
 }
