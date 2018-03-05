b24e012da80602f5909369c95297cc37c460db50
Kennedy Rose <kennedy@kennedyrose.com>
2018-03-05 12:40:43 -0600

    No default options

--- packages/gatsby-plugin-zygote/src/gatsby-browser.js
+++ packages/gatsby-plugin-zygote/src/gatsby-browser.js
@@ -1,9 +1,5 @@
-export function onInitialClientRender(a, options) {
+export function onInitialClientRender(a, options = {}) {
 	if('zygote' in window){
-		options = {
-			api: process.env.ZYGOTE_API || process.env.GATSBY_ZYGOTE_API || `https://yh5fc30fhh.execute-api.us-east-1.amazonaws.com/production/handler`,
-			...options
-		}
 		for(let i in options){
 			window.zygote[i] = options[i]
 		}
