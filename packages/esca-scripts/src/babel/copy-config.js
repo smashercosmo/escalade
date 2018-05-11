5c18c4471d360cb8fd8d0458f6065dced6fffb58
Kennedy Rose <kennedy@kennedyrose.com>
2018-05-11 08:52:14 -0500

    babel-jest

--- packages/esca-scripts/src/babel/copy-config.js
+++ packages/esca-scripts/src/babel/copy-config.js
@@ -3,9 +3,9 @@ import { outputJson } from 'fs-extra'
 import createConfig from './config'
 
 async function copyConfig(options){
-	console.log('Copying Babel config...')
+	console.log(`Copying Babel config...`)
 	const config = createConfig(options)
-	await outputJson(`.babelrc`, config, { spaces: '\t' })
+	await outputJson(`.babelrc`, config, { spaces: `\t` })
 }
 
 export default copyConfig
