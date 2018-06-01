bafe546b0c49e4b29840aba83d6383dd0b114bda
Kennedy Rose <kennedy@kennedyrose.com>
2018-06-01 08:34:49 -0500

    SSR

--- packages/gatsby-plugin-recaptcha/src/gatsby-ssr.js
+++ packages/gatsby-plugin-recaptcha/src/gatsby-ssr.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
-export function onRenderBody({ setPostBodyComponents }, pluginOptions) {
-	if (process.env.NODE_ENV === 'production' && pluginOptions.id) {
-		return setPostBodyComponents([
-			<script key='gatsby-plugin-hubspot' type='text/javascript' id='hs-script-loader' async defer src={`//js.hs-scripts.com/${pluginOptions.id}.js`}></script>
-		])
+export function onRenderBody({ setPostBodyComponents }, userOptions) {
+	const options = {
+		async: true,
+		defer: true,
+		args: ``,
+		...userOptions,
 	}
+	return setPostBodyComponents([
+		<script
+			type='text/javascript'
+			async={options.async}
+			defer={options.defer}
+			src={`https://www.google.com/recaptcha/api.js${options.args}`}
+			/>
+	])
 }
