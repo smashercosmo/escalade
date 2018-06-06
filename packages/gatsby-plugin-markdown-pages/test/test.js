fb2a44ee577027718b0d5bfbe1a8701b4dd9e257
Kennedy Rose <kennedy@kennedyrose.com>
2018-06-06 13:18:31 -0500

    Initial commit

---
+++ packages/gatsby-plugin-markdown-pages/test/test.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai'
+import { onRouteUpdate } from '../gatsby-browser'
+import { onRenderBody } from '../gatsby-ssr'
+
+describe('Gatsby plugins', () => {
+	it('should import', () => {
+	})
+})
