552bb24171c4772975d7420b1775c58b72634d0b
Kennedy Rose <kennedy@kennedyrose.com>
2018-07-05 09:18:22 -0500

    SSR

--- packages/gatsby-plugin-html-attributes/src/gatsby-ssr.js
+++ packages/gatsby-plugin-html-attributes/src/gatsby-ssr.js
@@ -1,9 +1,5 @@
-import React from 'react'
-
-export function onRenderBody({ setPostBodyComponents }, pluginOptions) {
-	if (process.env.NODE_ENV === 'production' && pluginOptions.id) {
-		return setPostBodyComponents([
-			<script key='gatsby-plugin-hubspot' type='text/javascript' id='hs-script-loader' async defer src={`//js.hs-scripts.com/${pluginOptions.id}.js`}></script>
-		])
-	}
+exports.onRenderBody = ({ setHtmlAttributes }, userOptions) => {
+	const options = { ...userOptions }
+	delete options.plugins
+	setHtmlAttributes(options)
 }
