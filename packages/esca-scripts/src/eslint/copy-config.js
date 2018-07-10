f6d7bca3656fcadc6e37a7bd4945c86cbb0e804f
Kennedy Rose <kennedy@kennedyrose.com>
2018-07-10 14:48:52 -0500

    ESLint json extension

--- packages/esca-scripts/src/eslint/copy-config.js
+++ packages/esca-scripts/src/eslint/copy-config.js
@@ -4,7 +4,7 @@ import createConfig from './config'
 async function copyConfig(options){
 	console.log(`Copying ESLint config...`)
 	const config = createConfig(options)
-	await outputJson(`.eslint`, config, { spaces: '\t' })
+	await outputJson(`.eslintrc.json`, config, { spaces: '\t' })
 }
 
 export default copyConfig
