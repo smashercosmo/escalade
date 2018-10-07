854ef0035c7c781dc10523896e9bb7f4069242bb
Zlatko Zlatev <zlatko@ralev.com>
2018-10-07 21:01:33 +0300

    yaml node support; deep walk frontmatter

--- packages/gatsby-plugin-netlify-cms-paths/src/gatsby-node.js
+++ packages/gatsby-plugin-netlify-cms-paths/src/gatsby-node.js
@@ -1,13 +1,63 @@
 const makeRelative = require(`./make-relative`)
 
-exports.onCreateNode = async ({ node }, options) => {
-	if (node.internal.type === `MarkdownRemark`) {
-		const fm = node.frontmatter
-		if(typeof fm === `object`){
-			for(let i in fm){
-				if(i === `_PARENT` || i === `parent`) continue
-				fm[i] = await makeRelative(node.fileAbsolutePath, fm[i], options)
-			}
+const walkObject = async (obj, iteratee, ignoreKeys = []) => {
+	for (let prop in obj) {
+		if ( ignoreKeys.indexOf(prop) !== -1 ) {
+			continue;
 		}
+
+		switch (typeof obj[prop]) {
+			case 'array':
+				obj[prop] = await walkArray(obj[prop], iteratee, ignoreKeys)
+				break;
+			case 'object':
+				obj[prop] = await walkObject(obj[prop], iteratee, ignoreKeys)
+				break;
+			default:
+				obj[prop] = await iteratee(obj[prop])
+		}
+	}
+
+	return obj
+}
+
+const walkArray = async (arr, iteratee, ignoreKeys = []) => {
+	return arr.map( async (i) => {
+		let result
+
+		switch (typeof i) {
+			case 'array':
+				result = await walkArray(i, iteratee, ignoreKeys)
+				break;
+			case 'object':
+				result = await walkObject(i, iteratee, ignoreKeys)
+				break;
+			default:
+				result = await iteratee(i)
+		}
+
+		return result
+	} )
+}
+
+exports.onCreateNode = async ({ node, getNode }, options) => {
+	const commonProps = ['id', '_PARENT', 'parent', 'children', 'internal']
+
+	let nodeAbsPath
+
+	const iteratee = async (val) => {
+		return await makeRelative(nodeAbsPath, val, options)
+	}
+	
+	if (node.internal.type === `MarkdownRemark`) {
+		nodeAbsPath = node.fileAbsolutePath
+
+		if(typeof node.frontmatter === `object`) {
+			await walkObject(node.frontmatter, iteratee, commonProps)
+		}
+	} else if (/^\w+Yaml$/.test(node.internal.type) ) {
+		nodeAbsPath = getNode(node.parent).absolutePath
+
+		await walkObject(node, iteratee, commonProps)
 	}
 }
