899e8d4a10cce2bfe26b11ce6cb79f6454973867
brighthero <matthias.hamborg@gmail.com>
2018-11-13 16:38:52 +0100

    make public_folder rewriteable

--- packages/gatsby-plugin-netlify-cms-paths/src/get-config.js
+++ packages/gatsby-plugin-netlify-cms-paths/src/get-config.js
@@ -7,7 +7,7 @@ let obj
 module.exports = async function({ cmsConfig = `/static/admin/config.yml` }){
 	if(obj) return obj
 	const configPath = join(cwd, cmsConfig)
-	const { media_folder, public_folder } = await readYaml(configPath)
+	let { media_folder, public_folder } = await readYaml(configPath)
 	if(!media_folder ){
 		console.error(`Missing media_folder in Netlify CMS config`)
 		process.exit(1)
