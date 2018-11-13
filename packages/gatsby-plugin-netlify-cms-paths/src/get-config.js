7e764c290949b66c182e409fd427cd2e77643787
brighthero <matthias.hamborg@gmail.com>
2018-11-13 16:38:52 +0100

    Revert "make public_folder rewriteable"
    This reverts commit 40f0b9a5866db757a1629233e1c8fb7ccf34f32b.

--- packages/gatsby-plugin-netlify-cms-paths/src/get-config.js
+++ packages/gatsby-plugin-netlify-cms-paths/src/get-config.js
@@ -7,7 +7,7 @@ let obj
 module.exports = async function({ cmsConfig = `/static/admin/config.yml` }){
 	if(obj) return obj
 	const configPath = join(cwd, cmsConfig)
-	let { media_folder, public_folder } = await readYaml(configPath)
+	const { media_folder, public_folder } = await readYaml(configPath)
 	if(!media_folder ){
 		console.error(`Missing media_folder in Netlify CMS config`)
 		process.exit(1)
