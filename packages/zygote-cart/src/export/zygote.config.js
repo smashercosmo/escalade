084a4d1bba8287ff9b79b6d027ffb579626ae5a0
Matt Suhay <matthew.suhay@gmail.com>
2019-01-18 13:50:03 -0600

    Removing plugin

--- packages/zygote-cart/src/export/zygote.config.js
+++ packages/zygote-cart/src/export/zygote.config.js
@@ -5,6 +5,6 @@ module.exports = {
 	splitName: true, // Split first name into 2 fields (first name, last name) {true}, or use one signle name field {false !default}
 	plugins: [
 		require('./plugins/zygote-plugin-standard-billing'), // Turn on the standard billing plugin to replace stripe's input
-		require('@escaladesports/zygote-plugin-esca-api/src'), // Working example plugin
+		// require('@escaladesports/zygote-plugin-esca-api/src'), // Working example plugin
 	],
 }
