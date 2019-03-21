4bd8c61dfc63a7095d86e2c05b61eee25a1684f8
Matt Suhay <matthew.suhay@gmail.com>
2019-03-21 08:53:59 -0500

    Turning off plugins

--- packages/zygote-cart/src/export/zygote.config.js
+++ packages/zygote-cart/src/export/zygote.config.js
@@ -3,7 +3,7 @@
 
 module.exports = {
 	plugins: [
-		require('./plugins/zygote-plugin-standard-billing'), // Turn on the standard billing plugin to replace stripe's input
-		require('@escaladesports/zygote-plugin-esca-api'), // Working example plugin
+		// require('./plugins/zygote-plugin-standard-billing'), // Turn on the standard billing plugin to replace stripe's input
+		// require('@escaladesports/zygote-plugin-esca-api'), // Working example plugin
 	],
 }
