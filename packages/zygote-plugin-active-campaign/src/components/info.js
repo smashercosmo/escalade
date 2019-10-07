dd2cce9bc5a09f514ef620f5504bf60e5f3df9ae
codevelopit <codevelopit@gmail.com>
2019-10-07 14:53:47 -0400

    injecting component in info

---
+++ packages/zygote-plugin-active-campaign/src/components/info.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function Info () {
+    return (
+        <div>
+          Would you like to receive the latest news from Victory Tailgate?
+        </div>
+      )
+  }
