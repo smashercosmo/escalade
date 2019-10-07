f945e5a1bf8822e82292474a2da77c30f8628d69
daelin9000 <nyoungpeter@gmail.com>
2019-10-07 01:25:48 -0400

    Changed object inits to fit awaits

--- packages/zygote-plugin-active-campaign/src/info.js
+++ packages/zygote-plugin-active-campaign/src/info.js
@@ -25,31 +25,32 @@ const preInfo = async ({ preFetchData, info }) => {
 	try {	
 
 		// init an active campaign connection
-		const acConnection = await new ActiveCampaignConnection().init()
+		const acConnection = new ActiveCampaignConnection()
+		acConnection = await acConnection.init()
 		console.log(`acConnection: `, acConnection)
 		if (!acConnection) return info
 
 		// init an active campaign contact
-		const acContact =
-			await new ActiveCampaignContact(
-				getContactProps(info)
-			).init()
+		const acContact = new ActiveCampaignContact(
+			getContactProps(info)
+		)
+		acContact = await acContact.init()
 		console.log(`acContact: `, acContact)
 		if (!acContact) return info
 
 		// init an active campaign e-commerce customer
-		const acCustomer =
-			await new ActiveCampaignEComCustomer(
-				getCustomerProps(info, acConnection, acceptsMarketing)
-			).init()
+		const acCustomer = new ActiveCampaignEComCustomer(
+			getCustomerProps(info, acConnection, acceptsMarketing)
+		)
+		acCustomer = await acCustomer.init()
 		console.log(`acCustomer: `, acCustomer)
 		if (!acCustomer) return info
 
 		// init an active campaign e-commerce order
-		const acOrder =
-			await new ActiveCampaignEComOrder(
-				getOrderProps(info, acConnection, acCustomer)
-			).createAbandonedOrder()
+		const acOrder = new ActiveCampaignEComOrder(
+			getOrderProps(info, acConnection, acCustomer)
+		)
+		acOrder = await acOrder.createAbandonedOrder()
 		console.log(`acOrder: `, acOrder)
 		if (!acOrder) return info
 
