088656a2dcbcc88dc89b53d6f2670f8c6f1eae0c
codevelopit <codevelopit@gmail.com>
2019-10-08 10:02:18 -0400

    added emotion core as a dependency and fixed some indentation

--- packages/zygote-plugin-active-campaign/src/components/info.js
+++ packages/zygote-plugin-active-campaign/src/components/info.js
@@ -5,34 +5,34 @@ import activeCampaignState from '../../state'
 
 
 function setMarketingState() {
-    let acceptsMkt = activeCampaignState.state.acceptsMarketing || false
-    console.log('Update marketing state: ', acceptsMkt)
-    activeCampaignState.setState({ acceptsMarketing: !acceptsMkt })
-    console.log(activeCampaignState)
+	let acceptsMkt = activeCampaignState.state.acceptsMarketing || false
+	console.log('Update marketing state: ', acceptsMkt)
+	activeCampaignState.setState({ acceptsMarketing: !acceptsMkt })
+	console.log(activeCampaignState)
 }
 
 const Info = () => {
-    return (
-        <Subscribe to={activeCampaignState}>
-            {state => (
-                <div css={styles.checkMarketing}>
-                    <input type="checkbox" id="checkbox_mkt" 
-                      onChange={setMarketingState}
-                      checked={state.acceptsMarketing}
-                    />
-                    <label htmlFor="checkbox_mkt">I would like to receive emails and updates about my order and special promotions</label>
-                </div>
-            )}
-        </Subscribe>  
-      )
+	return (
+		<Subscribe to={activeCampaignState}>
+			{state => (
+				<div css={styles.checkMarketing}>
+					<input type="checkbox" id="checkbox_mkt"
+						onChange={setMarketingState}
+						checked={state.acceptsMarketing}
+					/>
+					<label htmlFor="checkbox_mkt">I would like to receive emails and updates about my order and special promotions</label>
+				</div>
+			)}
+		</Subscribe>  
+	)
 }
 
 const styles = {
-    checkMarketing: css`
+	checkMarketing: css`
         padding: 5px 0px;
         margin-top: 25px;
         text-align: center;
-    `
+    `,
 }
 
 
