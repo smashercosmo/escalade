0edb8c6d9a145f00c217c241b7b9fd5bd51b5597
Abdelrahman Ashraf <a.theashraf@gmail.com>
2019-10-10 03:56:09 +0200

    refactor: 💡 use react hooks

--- packages/statable/dev/button.js
+++ packages/statable/dev/button.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react'
-import { Subscribe } from '../src'
+import React from "react";
+import { Subscribe } from "../src";
 
-import counter from './counter'
-import counter2 from './counter-2'
+import counter from "./counter";
+import counter2 from "./counter-2";
 
-export default class Counter extends Component {
-	render() {
-		return (
-			<Subscribe to={counter}>
-				{state => (
-					<div>
-						<button onClick={counter.increment}>
-							Progress: {state.progress}
-						</button>
-					</div>
-				)}
-			</Subscribe>
-		)
-	}
+export default function Counter() {
+  return (
+    <Subscribe to={counter}>
+      {state => (
+        <div>
+          <button onClick={counter.increment}>
+            Progress: {state.progress}
+          </button>
+        </div>
+      )}
+    </Subscribe>
+  );
 }
