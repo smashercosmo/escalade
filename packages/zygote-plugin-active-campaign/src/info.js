5143312a4a831deb7dcb6acfd41ad99381fe2d62
daelin9000 <nyoungpeter@gmail.com>
2019-10-13 23:22:06 -0400

    work is now done in the background, does not hang up the cart

--- packages/zygote-plugin-active-campaign/src/info.js
+++ packages/zygote-plugin-active-campaign/src/info.js
@@ -40,41 +40,47 @@ const preInfo = async ({ preFetchData, info }) => {
 	const acceptsMarketing = acState.state.acceptsMarketing ? `1` : `0`
 	console.log(`Accepts Marketing: `, acceptsMarketing)
 	
-	try {	
+	const sendData = async () => { 
+		try {
 
-		// init an active campaign connection
-		let acConnection = new Connection()
-		acConnection = await acConnection.init()
-		console.log(`acConnection: `, acConnection)
-		if (!acConnection) return info
+			// init an active campaign connection
+			let acConnection = new Connection()
+			acConnection = await acConnection.init()
+			console.log(`acConnection: `, acConnection)
+			if (!acConnection) return info
 
-		// init an active campaign contact
-		let acContact = new Contact(
-			getContactProps(info)
-		)
-		acContact = await acContact.init()
-		console.log(`acContact: `, acContact)
-		if (!acContact) return info
+			// init an active campaign contact
+			let acContact = new Contact(
+				getContactProps(info)
+			)
+			acContact = await acContact.init()
+			console.log(`acContact: `, acContact)
+			if (!acContact) return info
 
-		// init an active campaign e-commerce customer
-		let acCustomer = new EComCustomer(
-			getCustomerProps(info, acConnection, acceptsMarketing)
-		)
-		acCustomer = await acCustomer.init()
-		console.log(`acCustomer: `, acCustomer)
-		if (!acCustomer) return info
+			// init an active campaign e-commerce customer
+			let acCustomer = new EComCustomer(
+				getCustomerProps(info, acConnection, acceptsMarketing)
+			)
+			acCustomer = await acCustomer.init()
+			console.log(`acCustomer: `, acCustomer)
+			if (!acCustomer) return info
 
-		// init an active campaign e-commerce order
-		let acOrder = new EComOrder(
-			getOrderProps(info, acConnection, acCustomer)
-		)
-		acOrder = await acOrder.createAbandonedOrder()
-		console.log(`acOrder: `, acOrder)
-		if (!acOrder) return info
+			// init an active campaign e-commerce order
+			let acOrder = new EComOrder(
+				getOrderProps(info, acConnection, acCustomer)
+			)
+			acOrder = await acOrder.createAbandonedOrder()
+			console.log(`acOrder: `, acOrder)
+			if (!acOrder) return info
 
-	} catch (ex) {
-		console.error(`ZygoteAC Error!: `, ex)
+		} catch (ex) {
+			console.error(`ZygoteAC Error!: `, ex)
+		}
 	}
+
+	// dont `await`, let it run in the background
+	sendData()
+	
 	return info
 }
 
