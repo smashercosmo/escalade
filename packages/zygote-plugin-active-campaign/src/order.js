8dcb3df4d1aa84b38412ff7586b842007d52015b
codevelopit <codevelopit@gmail.com>
2019-10-31 10:29:04 -0400

    logging post order data received

--- packages/zygote-plugin-active-campaign/src/order.js
+++ packages/zygote-plugin-active-campaign/src/order.js
@@ -7,6 +7,9 @@ const postOrder = async ({response, info, preFetchData}) => {
         // don't await - let it run in background
         console.log(`completing AC order...`)
         console.log(`Complete order data in state: `, acState.state)
+        console.log(`Response from postOrder api: `, response)
+        console.log(`Info from postOrder api: `, info)
+        console.log(`preFetchData from postOrder api: `, preFetchData)
         completeAbandonedStateOrder()
 
     } catch (ex) {
