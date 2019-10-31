febdfb60fc5b0cac9b63cb40110e7fdbded5e1bd
codevelopit <codevelopit@gmail.com>
2019-10-31 11:33:43 -0400

    updated to check if response is success before completing order

--- packages/zygote-plugin-active-campaign/src/order.js
+++ packages/zygote-plugin-active-campaign/src/order.js
@@ -2,6 +2,9 @@ import { completeAbandonedStateOrder } from './classes'
 import acState from '../state'
 
 const postOrder = async ({response, info, preFetchData}) => {
+
+    const { success } = response
+    // Only complete if order was successfully submitted
     try {
         // update current order as unabandoned
         // don't await - let it run in background
@@ -10,11 +13,11 @@ const postOrder = async ({response, info, preFetchData}) => {
         console.log(`Response from postOrder api: `, response)
         console.log(`Info from postOrder api: `, info)
         console.log(`preFetchData from postOrder api: `, preFetchData)
-        completeAbandonedStateOrder()
-
+        if (success) completeAbandonedStateOrder()
     } catch (ex) {
         console.error(`Error!: `, ex)
     }
+
     return response
 }
 
