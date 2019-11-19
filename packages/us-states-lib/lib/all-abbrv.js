bdfff0848038d992eaa2b51937885222b2a9249e
codevelopit <codevelopit@gmail.com>
2019-11-19 14:32:13 -0500

    added scripts and updated project structure

--- packages/us-states-lib/lib/all-abbrv.js
+++
@@ -1,8 +0,0 @@
-exports.default = void 0
-var dc = require('./dc-abbrv.json')
-var insular = require('./insular-abbrv.json')
-var state = require('./state-abbrv.json')
-
-var _list = { ...dc,  ...insular, ...state }
-exports["default"] = _list
-module.exports = exports["default"]
