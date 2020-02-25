93eb785130bd3e34d2889c25ae967fafb2594ff3
tbaustin <tbaustin1992@gmail.com>
2020-02-25 10:12:37 -0600

    move react-power-reviewsinto packages

--- needs-updated/react-power-reviews/plugins/email-templates/gatsby-node.js
+++
@@ -1,5 +0,0 @@
-const postProcess = require(`./post-process`)
-
-exports.onPostBuild = async (_, { files, siteUrl }) => {
-	await postProcess(files, siteUrl)
-}
--- needs-updated/react-power-reviews/src/styles/fonts.js
+++
@@ -1,6 +0,0 @@
-const fonts = {
-	primaryFont: `oswald`,
-	secondaryFont: `open sans`,
-}
-
-export default fonts
