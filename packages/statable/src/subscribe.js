c5e9c1b0de3311b19547a364c05251b3fec08e51
tbaustin <tbaustin1992@gmail.com>
2020-02-25 10:19:17 -0600

    add statable into packages

---
+++ packages/statable/src/subscribe.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react"
+
+function Subscribe(props) {
+	const [, setState] = useState({})
+	let multi = false
+
+	if (Array.isArray(props.to)) multi = true
+
+	function onChange() {
+		setState({ changed: true })
+	}
+
+	useEffect(() => {
+		if (!multi) {
+			props.to.subscribe(onChange)
+			return
+		}
+		props.to.forEach(to => to.subscribe(onChange))
+
+		return () => {
+			if (!multi) {
+				props.to.unsubscribe(onChange)
+				return
+			}
+			props.to.forEach(to => to.unsubscribe(onChange))
+		}
+	}, [])
+
+	if (!multi) return props.children(props.to.state)
+	return props.children(...props.to.map(to => to.state))
+}
+
+export default Subscribe
